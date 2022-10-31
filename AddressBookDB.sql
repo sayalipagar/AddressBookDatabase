bc6df7fb2512701c69b9a0a9224584fc3eefca41
sayalipagar <pagarsayali565@gmail.com>
2022-10-31 18:29:05 +0530

    [Sayali]:InsertValueInTable

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -15,3 +15,12 @@ Phone varchar(25),
 Email varchar(25)
 );
  SELECT * FROM ADDRESS_BOOK_SERVICE;
+
+  -----UC3-----
+ ----INSERT CONTACT IN ADDRESSBOOK------
+INSERT INTO ADDRESS_BOOK_SERVICE VALUES('Sayali','Nikam','Pune','Maharashtra','412109','8457842345','sayalinikam@gmail.com');
+INSERT INTO ADDRESS_BOOK_SERVICE VALUES('Ankita','Khairnar','Mumbai','Maharashtra','560085','954645345','ankitakhair@gmail.com');
+INSERT INTO ADDRESS_BOOK_SERVICE VALUES('Sandesh','Nikam','Pune','Maharashtra','8375','76378434534','sandesh@gmail.com');
+INSERT INTO ADDRESS_BOOK_SERVICE VALUES('snehal','saner','nashik','Maharashtra','574895','09384903','snaehal@gmail.com');
+INSERT INTO ADDRESS_BOOK_SERVICE VALUES('Priyanka','sonawane','hyderabad','Telangana','455454','0938490334','priyanka@gmail.com');
+SELECT * FROM ADDRESS_BOOK_SERVICE;
