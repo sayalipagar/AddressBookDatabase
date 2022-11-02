d80a7ca29a21beb327ed16628fb0f847428a5b4e
sayalipagar <pagarsayali565@gmail.com>
2022-11-02 21:25:16 +0530

    [Sayali]Add:IdentifyAddressbook

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -48,3 +48,9 @@ Select COUNT(StateName) as State_Count FROM ADDRESS_BOOK_SERVICE;
 ------------UC8-Sorting data Alphabetically by person name------------
 
 SELECT * FROM ADDRESS_BOOK_SERVICE ORDER BY(FirstName);
+
+-----------UC9-Ability to identify each Address Book with name and Type---------
+alter table ADDRESS_BOOK_SERVICE  add AddressBook_Name varchar(30),AddressBook_Type varchar(30);
+update ADDRESS_BOOK_SERVICE set AddressBook_Name = 'Family address book', AddressBook_Type = 'Family' where FirstName='Ankita';
+update ADDRESS_BOOK_SERVICE set AddressBook_Name = 'Friends address book', AddressBook_Type = 'Friends' where FirstName='Sandesh';
+select * from ADDRESS_BOOK_SERVICE;
